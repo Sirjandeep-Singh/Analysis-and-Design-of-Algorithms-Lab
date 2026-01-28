5a147730c4ca4ab12b3250bfdc1075eb4d2799fd
Sirjandeep-Singh <sirjandeepsinghkhrod379@gmail.com>
2026-01-28 14:22:35 +0530

    completed a single missing Algorithm and Changed File Structure.

--- Lab-Programs/src/LAB1/EarlyDupe.java
+++ Lab-Programs/src/LAB1/EarlyDupe.java
@@ -1,15 +1,13 @@
 package LAB1;
 
 public class EarlyDupe {
-    private static int earliestDuplicate(int[] arr, int limit){
+    private static int earliestDuplicate(int[] arr){
         int size = arr.length;
         int uppermid = (size + 1)/2;
-        int[] hashFreq = new int[limit + 1];
-        for(int i = 0 ; i < uppermid; i++){
-            if(hashFreq[arr[i]] == 1){
-                return i;
+        for(int i = 0 ; i < arr.length/2; i++){
+            for(int j = 0 ; j < arr.length/2 ; j++){
+                if(arr[i] == arr[j]) return j;
             }
-            hashFreq[arr[i]]++;
         }
         return uppermid;
     }
@@ -17,6 +15,6 @@ public class EarlyDupe {
     public static void main(String[] args){
 
         int[] arr = {1, 2, 5, 1, 4, 4, 3, 2, 3, 5};
-        System.out.println(earliestDuplicate(arr, 5));
+        System.out.println(earliestDuplicate(arr));
     }
 }
